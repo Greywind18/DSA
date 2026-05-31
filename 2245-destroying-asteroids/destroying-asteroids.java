1638cb6944c383827d4cf07ce967bf8939d40326
Dhruv Khandelwal <59694554+Greywind18@users.noreply.github.com>
2026-05-31 17:32:21 +0530

    Time: 8 ms (96.54%) | Memory: 106.1 MB (91.92%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -1,15 +1,34 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-        Arrays.sort(asteroids);
-        boolean ans = true;
-        long planetMass = mass;
-        for(int i=0;i<asteroids.length;i++){
-            if(asteroids[i]<=planetMass){
-                planetMass+=asteroids[i];
-            }else{
-                return false;
+        int maxAsteroid = 0;
+        int n = asteroids.length;
+        for(int i=0;i<n;i++){
+            if(asteroids[i]>maxAsteroid){
+                maxAsteroid = asteroids[i]; 
             }
         }
-        return ans;
+        int []freq = new int[maxAsteroid+1];
+        for(int i=0;i<n;i++){
+            freq[asteroids[i]]++;
+        }
+        long currentMass = mass;
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>0){
+                if(i>currentMass) return false;
+                currentMass+=(long)i*freq[i];
+            }
+        }
+        return true;
+        // Arrays.sort(asteroids);
+        // boolean ans = true;
+        // long planetMass = mass;
+        // for(int i=0;i<asteroids.length;i++){
+        //     if(asteroids[i]<=planetMass){
+        //         planetMass+=asteroids[i];
+        //     }else{
+        //         return false;
+        //     }
+        // }
+        // return ans;
     }
 }
