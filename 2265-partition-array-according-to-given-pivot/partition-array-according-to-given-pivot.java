5628dd3d66e56cbe3c9c24ba4584da64951027da
Dhruv Khandelwal <59694554+Greywind18@users.noreply.github.com>
2026-06-08 16:17:26 +0530

    Time: 4 ms (99.24%) | Memory: 159.7 MB (73.16%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,31 +1,22 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int pivotCnt = 0;
-        ArrayList<Integer> pre = new ArrayList<>();
-        ArrayList<Integer> post = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==pivot){
-                pivotCnt++;
-            }else if(nums[i]<pivot){
-                pre.add(nums[i]);
-            }else if(nums[i]>pivot){
-                post.add(nums[i]);
-            }
+        int n = nums.length;
+       int []ans = new int[n];
+       int left=0, right=n-1;
+       int i=0,j = n-1;
+       while(i<n){
+        if(nums[i]<pivot){
+            ans[left++] = nums[i];
         }
-        int n = post.size();
-        int j = 0;
-        while(j<n || pivotCnt>0){
-            if(pivotCnt>0){
-                pre.add(pivot);
-                pivotCnt--;
-            }else{
-                pre.add(post.get(j));
-                j++;
-            }
+        if(nums[j]>pivot){
+            ans[right--] = nums[j];
         }
-        int[] ans = pre.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        i++;
+        j--;
+       }
+       while(left<=right){
+        ans[left++] = pivot;
+       }
        return ans;
     }
 }
